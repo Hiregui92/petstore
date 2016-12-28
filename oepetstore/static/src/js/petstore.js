1ac1072ebab0e987382bab14d1ed204c96835b04
hembert <hembert@vauxoo.com>
2016-12-28 17:07:41 +0000

    [ADD] petstore: Exercise. Usage of QWeb in Widgets

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -5,9 +5,22 @@ openerp.oepetstore = function(instance, local) {
 
     local.HomePage = instance.Widget.extend({
         start: function() {
-            console.log("pet store home page loaded");
+            var products = new local.ProductsWidget(
+                this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
+            products.appendTo(this.$el);
         },
     });
 
-    instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
+    local.ProductsWidget = instance.Widget.extend({
+        template: "ProductsWidget",
+        init: function(parent, products, color) {
+            this._super(parent);
+            console.log(products);
+            this.products = products;
+            this.color = color;
+        },
+    });
+
+    instance.web.client_actions.add(
+        'petstore.homepage', 'instance.oepetstore.HomePage');
 }
