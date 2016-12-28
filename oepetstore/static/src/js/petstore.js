2fdf728c344650604e0cd648cb0eaf1d836f2040
hembert <hembert@vauxoo.com>
2016-12-28 19:44:44 +0000

    [ADD] petstore: Exercise. Message of the Day

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -2,16 +2,28 @@ openerp.oepetstore = function(instance, local) {
     var _t = instance.web._t,
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
+
     local.HomePage = instance.Widget.extend({
-      start: function() {
-          var self = this;
-          var model = new instance.web.Model("oepetstore.message_of_the_day");
-          model.call("my_method", {context: new instance.web.CompoundContext({'new_key': 'key_value'})}).then(function(result) {
-              self.$el.append("<div>Hello " + result["hello"] + "</div>");
-              // will show "Hello world" to the user
-          });
-      },
+        template: "HomePage",
+        start: function() {
+            return new local.MessageOfTheDay(this).appendTo(this.$el);
+        },
     });
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
+
+    local.MessageOfTheDay = instance.Widget.extend({
+        template: "MessageOfTheDay",
+        start: function() {
+            var self = this;
+            return new instance.web.Model("oepetstore.message_of_the_day")
+                .query(["message"])
+                .order_by('-create_date', '-id')
+                .first()
+                .then(function(result) {
+                    self.$(".oe_mywidget_message_of_the_day").text(result.message);
+                });
+        },
+    });
+
 }
