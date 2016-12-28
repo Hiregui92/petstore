6850707fe9c98f993e4471e26ce2a968e5c472f8
hembert <hembert@vauxoo.com>
2016-12-28 17:26:43 +0000

    [ADD] petstore: Example. Widget Events and Properties

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -4,11 +4,24 @@ openerp.oepetstore = function(instance, local) {
     var QWeb = instance.web.qweb;
 
     local.HomePage = instance.Widget.extend({
+//        start: function() {
+//            var products = new local.ProductsWidget(
+//                this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
+//            products.appendTo(this.$el);
+//        },
+
         start: function() {
-            var products = new local.ProductsWidget(
-                this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
-            products.appendTo(this.$el);
-        },
+            var widget = new local.ConfirmWidget(this);
+            widget.on("user_chose", this, this.user_chose);
+            widget.appendTo(this.$el);
+         },
+         user_chose: function(confirm) {
+            if (confirm) {
+               console.log("The user agreed to continue");
+            } else {
+               console.log("The user refused to continue");
+            }
+         },
     });
 
     local.ProductsWidget = instance.Widget.extend({
@@ -18,6 +31,22 @@ openerp.oepetstore = function(instance, local) {
             console.log(products);
             this.products = products;
             this.color = color;
+        },
+    });
+
+    local.ConfirmWidget = instance.Widget.extend({
+        events: {
+            'click button.ok_button': function () {
+                this.trigger('user_chose', true);
+            },
+            'click button.cancel_button': function () {
+                this.trigger('user_chose', false);
+            }
+        },
+        start: function() {
+            this.$el.append("<div>Are you sure you want to perform this action?</div>" +
+                "<button class='ok_button'>Ok</button>" +
+                "<button class='cancel_button'>Cancel</button>");
         },
     });
 
